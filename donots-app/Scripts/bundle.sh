74a45231e8cb0c07144c711c3a771492b846aeaf
Michael Bino <mabino@gmail.com>
2026-03-20 21:46:20 -0400

    feat: use APP_VERSION in bundle scripts for Info.plist versioning

--- donots-app/Scripts/bundle.sh
+++ donots-app/Scripts/bundle.sh
@@ -23,6 +23,17 @@ cp "${BUILD_DIR}/${APP_NAME}" "${MACOS_DIR}/${APP_NAME}"
 # Copy Info.plist
 cp "Resources/Info.plist" "${CONTENTS_DIR}/Info.plist"
 
+# Determine version
+VERSION="${APP_VERSION:-1.0.0}"
+if [[ -z "${APP_VERSION:-}" && -f "../VERSION" ]]; then
+    VERSION=$(tr -d '[:space:]' < "../VERSION")
+elif [[ -z "${APP_VERSION:-}" && -f "VERSION" ]]; then
+    VERSION=$(tr -d '[:space:]' < "VERSION")
+fi
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VERSION" "${CONTENTS_DIR}/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $VERSION" "${CONTENTS_DIR}/Info.plist"
+
 # Copy entitlements
 cp "Resources/Donots.entitlements" "${RESOURCES_DIR}/Donots.entitlements"
 
